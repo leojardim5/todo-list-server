83c7916c83e2086fc3a1e18e6bf1d15ffcdd739b
Leonardo Jardim <leojardim5@gmail.com>
2023-03-12 22:54:04 -0300

    nnn

--- taskdb.js
+++ taskdb.js
@@ -4,7 +4,7 @@ const connection = require('./dbConnection')
 const taskSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true 
   },
   about: {
     type: String,
